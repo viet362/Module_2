423462a0198be91c41b82485755258ac06a4ec6e
viet362 <buibietbacn01@gmail.com>
2026-01-12 21:47:24 +0700

    third commit

---
+++ Mang_Phuongthuc/src/TH_Mang.java
@@ -0,0 +1,194 @@
+import java.util.Scanner;
+
+public class TH_Mang {
+
+    //[Thực hành] Đảo ngược các phần tử của mảng (v.2023)
+    /*
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size does not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.printf("%-20s%s", "Elements in array: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+        for (int j = 0; j < array.length / 2; j++) {
+            int temp = array[j];
+            array[j] = array[size - 1 - j];
+            array[size - 1 - j] = temp;
+        }
+        System.out.printf("\n%-20s%s", "Reverse array: ", "");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+    }
+     */
+
+    //[Thực hành] Tìm giá trị trong mảng (v.2023)
+    /*
+    public static void main(String[] args) {
+        String[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a name’s student:");
+        String input_name = scanner.nextLine();
+
+        boolean isExist = false;
+        for (int i = 0; i < students.length; i++) {
+            if (students[i].equals(input_name)) {
+                System.out.println("Position of the students in the list " + input_name + " is: " + i);
+                isExist = true;
+                break;
+            }
+        }
+        if (!isExist) {
+            System.out.println("Not found" + input_name + " in the list.");
+        }
+    }
+     */
+
+    //[Thực hành] Tìm giá trị lớn nhất trong mảng (v.2023)
+    /*
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element" + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        System.out.print("Property list: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + " ,at position " + index);
+    }
+     */
+
+    //[Thực hành] Chương trình chuyển đổi nhiệt độ (v.2023)
+    /*
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double fahrenheit;
+        double celsius;
+        int choice;
+
+        do {
+            System.out.println("Menu.");
+            System.out.println("1. Fahrenheit to Celsius");
+            System.out.println("2. Celsius to Fahrenheit");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice: ");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1: {
+                    System.out.println("Enter fahrenheit: ");
+                    fahrenheit = input.nextDouble();
+                    System.out.println("Fahrenheit to Celsius: " + fahrenheitToCelsius(fahrenheit));
+                    break;
+                }
+                case 2: {
+                    System.out.println("Enter Celsius: ");
+                    celsius = input.nextDouble();
+                    System.out.println("Celsius to Fahrenheit: " + celsiusToFahrenheit(celsius));
+                    break;
+                }
+                case 0:
+                    System.exit(0);
+            }
+        } while (choice != 0);
+    }
+
+    public static double celsiusToFahrenheit(double celsius) {
+        double fahrenheit = (9.0 / 5) * celsius + 32;
+        return fahrenheit;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        double celsius = (5.0 / 9) * (fahrenheit - 32);
+        return celsius;
+    }
+     */
+
+    //[Thực hành] Tìm giá trị nhỏ nhất trong mảng sử dụng phương thức (v.2023)
+    /*
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+     */
+
+    //[*Thực hành] Ứng dụng đếm số lượng sinh viên thi đỗ (v.2023)
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 30)
+                System.out.println("Size should not exceed 30");
+        } while (size > 30);
+
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter a mark for student " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+
+        int count = 0;
+        System.out.print("List of mark: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+            if (array[j] >= 5 && array[j] <= 10)
+                count++;
+        }
+        System.out.print("\n The number of students passing the exam is " + count);
+    }
+}
+
+
--- VongLap/TH_Loop/src/BT_Loop.java
+++ VongLap/TH_Loop/src/BT_Loop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BT_Loop {
 
     //[Bài tập] Hiển thị 20 số nguyên tố đầu tiên
@@ -25,6 +27,7 @@ public class BT_Loop {
      */
 
     // [Bài tập] Hiển thị các số nguyên tố nhỏ hơn 100
+    /*
     public static void main(String[] args) {
         int max = 100;
         int number = 2;
@@ -44,8 +47,102 @@ public class BT_Loop {
             number++;
         }
     }
+     */
 
     // [Bài tập] Hiển thị các loại hình
-    //
+    public static void main(String[] args) {
+        int choice;
+        do {
+            System.out.println("==== Menu ==========");
+            System.out.println("1.Print the rectangle");
+            System.out.println("2.Print the square triangle");
+            System.out.println("3.Print isosceles triangle");
+            System.out.println("4.Exit");
+            Scanner input = new Scanner(System.in);
+
+            do{
+                System.out.print("\nSelect your option: ");
+                choice = input.nextInt();
+                if(choice<1 || choice>4)
+                    System.out.println("Error, try again!");
+            }while (choice<1 || choice>4);
+
+            switch (choice){
+                case 1:
+                    int chieudai = 12, chieurong = 5;
+                    for (int i = 0; i < chieurong; i++) {
+                        for (int j = 0; j < chieudai; j++) {
+                            System.out.print("*");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 2:
+                    int size = 5;
+                    System.out.println("Option:\n 1.top-left.\n 2.top-right.\n 3.botton-left.\n 4.botton-right");
+                    System.out.print("Select your option: ");
+                    int option = input.nextInt();
+                    System.out.print("\n");
+                    switch (option){
+                        case 1:
+                            for (int i = 1; i <= size; i++) {
+                                for (int j = 1; j <= i; j++) {
+                                    System.out.print("*");
+                                }
+                                System.out.print("\n");
+                            }
+                            break;
+                        case 2:
+                            for (int i = 1; i <= size; i++) {
+                                for (int j = size - i; j > 0; j--) {
+                                    System.out.print(" ");
+                                }
+                                for (int k = 1; k <= i; k++) {
+                                    System.out.print("*");
+                                }
+                                System.out.print("\n");
+                            }
+                            break;
+                        case 3:
+                            for (int i = size; i > 0; i--) {
+                                for (int j = size; j > size - i; j--) {
+                                    System.out.print("*");
+                                }
+                                System.out.print("\n");
+                            }
+                            break;
+                        case 4:
+                            for (int i = size; i > 0; i--) {
+                                for (int j = 0; j < size - i; j++) {
+                                    System.out.print(" ");
+                                }
+                                for (int k = size; k > size - i; k--) {
+                                    System.out.print("*");
+                                }
+                                System.out.print("\n");
+                            }
+                            break;
+                        default:
+                            System.out.println("Error\n");
+                    }
+                case 3:
+                    int size2 = 5;
+                    for (int i = 1; i <= size2; i++) {
+                        for (int j = size2 - i; j > 0; j--) {
+                            System.out.print(" ");
+                        }
+                        for (int k = 1; k < i*2; k++) {
+                            System.out.print("*");
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 4:
+                    System.out.println("Exit. Thanks!");
+                    break;
+            }
+            System.out.println("Done!\n");
+        }while (choice != 4);
+    }
 
 }
